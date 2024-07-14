7d8ae3cfdd9105b4d9568fbc7db946136efd01c4
Gurpreet singh <gp.juit@outlook.com>
2024-07-14 19:43:02 +0530

    first Commit

---
+++ GetYourMentor/GetYourMentor/GetYourMentorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetYourMentorApp.swift
+//  GetYourMentor
+//
+//  Created by Gurpreet Singh on 14/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GetYourMentorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
